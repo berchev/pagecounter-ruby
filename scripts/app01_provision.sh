f2946d14e73842c4bc112ac81e0f4326b7f632a9
Alvaro Miranda Aguilera <kikitux+github@gmail.com>
2019-06-08 10:43:29 +0200

    Update app01_provision.sh

--- scripts/app01_provision.sh
+++ scripts/app01_provision.sh
@@ -13,17 +13,25 @@ which jq || {
 gem list -i redis || gem install redis
 
 # Install redis tools
-which redis-cli || apt-get install -y redis-tools
+which redis-cli || {
+  apt-get update
+  apt-get install -y redis-tools
+}
 
 # Download and install latest version of vault if not installed
 [ -f "/usr/local/bin/vault" ] || {
   cd /usr/local/bin
   VAULT_URL=$(curl https://releases.hashicorp.com/index.json | jq '{vault}' | egrep "linux.*amd64" | sort -r | head -1 | awk -F[\"] '{print $4}')
-  curl -o vault.zip $VAULT_URL
+  curl -o vault.zip ${VAULT_URL}
   unzip vault.zip
   rm -f vault.zip
 }
 
 # Install vault autocomplete
-grep 'complete -C /usr/local/bin/vault vault' /home/vagrant/.bashrc || echo 'complete -C /usr/local/bin/vault vault' | tee -a /home/vagrant/.bashrc
-grep 'exec $SHELL' /home/vagrant/.bash_profile || echo 'exec $SHELL' | tee -a /home/vagrant/.bash_profile
+grep 'complete -C /usr/local/bin/vault vault' /home/vagrant/.bashrc || {
+  echo 'complete -C /usr/local/bin/vault vault' | tee -a /home/vagrant/.bashrc
+}
+
+grep 'exec $SHELL' /home/vagrant/.bash_profile || {
+  echo 'exec $SHELL' | tee -a /home/vagrant/.bash_profile
+}
